0b532d1e180d57f4c93307551a39a24c8e454541
Erick <19063989@student.hhs.nl>
2024-03-22 12:00:58 +0100

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,15 +5,23 @@ using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 /* --- Requirements ---
-The game can be in 3D or 2D, you get to pick.
-It consists of 1 scene. This scene has a 'floor' and a higher platform, like a 'table'.
-Like in Tetris, the player gets a random box that they can drop unto the table from a certain height.
-Important: dropping the boxes and their collision is Physics-based.
-The goal of the game is to stack the highest tower of boxes. If a box is dropped on the floor and not on the table or another box, it is game over.
-When the box lands, the player gets a new box (Again, like in Tetris)
-The boxes are of different shapes and colours (required to use PreFabs). Shapes can be inspired by Tetris or you can mix it up by using triangles, circles, capsules, etc. Only using square cubes isn't sufficient.
-Before the player drops the box they can move it around left-right horizontally using either arrow keys or mouse position (one of the options, both isn't required).
-If the tower of boxes gets higher and higher, make sure your camera responds by moving up or zooming out.
+- The game can be in 3D or 2D, you get to pick.
+- It consists of 1 scene. This scene has a 'floor' and a higher platform, like a 'table'.
+- Like in Tetris, the player gets a random box that they can drop unto the table from a certain height.
+- Important: dropping the boxes and their collision is Physics-based.
+- The goal of the game is to stack the highest tower of boxes. 
+If a box is dropped on the floor and not on the table or another box, it is game over.
+
+- When the box lands, the player gets a new box (Again, like in Tetris)
+
+- The boxes are of different shapes and colours (required to use PreFabs). 
+Shapes can be inspired by Tetris or you can mix it up by using triangles, 
+circles, capsules, etc. Only using square cubes isn't sufficient.
+
+- Before the player drops the box they can move it around left-right horizontally using either arrow keys or mouse position 
+(one of the options, both isn't required).
+
+- If the tower of boxes gets higher and higher, make sure your camera responds by moving up or zooming out.
 
 Tips:
 
