5e09b8c003cbb4662cb8e1dccb53e127614f4a15
Erick <19063989@student.hhs.nl>
2024-03-29 00:05:57 +0100

    Enable Disable, but it still does not work with enabling

--- Assets/MaxHeightLine.cs
+++ Assets/MaxHeightLine.cs
@@ -12,61 +12,64 @@ public class MaxHeightLine : MonoBehaviour
     void Start()
     {
         // Start the coroutine to toggle the script state
-        StartCoroutine(ToggleScript());
+       //Disable();
     }
 
-    // Coroutine to toggle the script state
-    IEnumerator ToggleScript()
-    {
-        while (true)
-        {
-            // Enable the script
-            enabled = true;
+    public void Enable(){
+        gameObject.SetActive(true);
+    }
 
-            // Wait for 2 seconds
-            yield return new WaitForSeconds(2f);
-
-            // Disable the script
-            enabled = false;
-
-            // Wait for 2 seconds
-            yield return new WaitForSeconds(2f);
-        }
+    public void Disable(){
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enabled)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            // Calculate the width of the object using its BoxCollider2D
-            float objectWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-
-            // Calculate the spacing between each ray
-            float raySpacing = objectWidth / (rayCount - 1);
-
-            // Cast rays downwards
-            hits = new RaycastHit2D[rayCount];
-            for (int i = 0; i < rayCount; i++)
+            if (gameObject.activeSelf == false)
             {
-                // Calculate the origin of each ray
-                Vector2 rayOrigin = new Vector2(transform.position.x - objectWidth / 2 + i * raySpacing, transform.position.y);
+                Enable();
+            }
+            else
+            {
+                Disable();
+            }
+        }
 
-                // Cast a ray downwards at the calculated origin
-                hits[i] = Physics2D.Raycast(rayOrigin, Vector2.down, distance, ~layerMask);
+        DrawRayDownwards();
+    }
 
-                // Draw debug lines
-                if (hits[i].collider != null)
-                {
-                    Debug.Log("Ray " + i + " hit: " + hits[i].collider.gameObject.name);
-                    Debug.DrawLine(rayOrigin, hits[i].point, Color.blue);
-                }
-                else
-                {
-                    Vector3 endPosition = rayOrigin + Vector2.down * distance;
-                    Debug.DrawLine(rayOrigin, endPosition, Color.red);
-                    Debug.Log("Ray " + i + " didn't hit anything");
-                }
+    private void DrawRayDownwards()
+    {
+        // Calculate the width of the object using its BoxCollider2D
+        float objectWidth = this.GetComponent<SpriteRenderer>().bounds.size.x;
+
+        // Calculate the spacing between each ray
+        float raySpacing = objectWidth / (rayCount - 1);
+
+        // Cast rays downwards
+        hits = new RaycastHit2D[rayCount];
+        for (int i = 0; i < rayCount; i++)
+        {
+            // Calculate the origin of each ray
+            Vector2 rayOrigin = new Vector2(transform.position.x - objectWidth / 2 + i * raySpacing, transform.position.y);
+
+            // Cast a ray downwards at the calculated origin
+            hits[i] = Physics2D.Raycast(rayOrigin, Vector2.down, distance, ~layerMask);
+
+            // Draw debug lines
+            if (hits[i].collider != null)
+            {
+                //Debug.Log("Ray " + i + " hit: " + hits[i].collider.gameObject.name);
+                Debug.DrawLine(rayOrigin, hits[i].point, Color.blue);
+            }
+            else
+            {
+                Vector3 endPosition = rayOrigin + Vector2.down * distance;
+                Debug.DrawLine(rayOrigin, endPosition, Color.red);
+                //Debug.Log("Ray " + i + " didn't hit anything");
             }
         }
     }
