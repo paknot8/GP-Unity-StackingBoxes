e371def561e88d5b2589966fdf129cc4d4386db3
Erick <19063989@student.hhs.nl>
2024-04-06 13:24:04 +0200

    Update GameUI.cs

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -3,18 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void RestartGame()
     {
         SceneManager.LoadScene(1);
