9c7a5b91ca89ef8e731624f966a4b2ae975aa5d2
Erick <19063989@student.hhs.nl>
2024-04-06 14:33:13 +0200

    Made Pause Menu Transparancy

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Import Unity UI namespace
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private TMPro.TextMeshProUGUI scoreText;
+    [SerializeField] private Image panelImage; // Use Image component for UI elements
 
     [SerializeField] private GameManager gameManager;
     [SerializeField] private GameObject GameScreenCanvas;
     [SerializeField] private GameObject GamePauseCanvas;
-    // [SerializeField] private GameObject GameOverCanvas;
 
-    void Update(){
+    [SerializeField] private float transparacy;
+
+    void Awake(){
+        transparacy = 0.8f;
+    }
+
+    void Start()
+    {
+        SetPanelTransparency(transparacy);
+    }
+
+    void Update()
+    {
         UpdateScoreText();
     }
 
@@ -28,19 +41,36 @@ public class GameUI : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void ResetValues(){
+    private void ResetValues()
+    {
         gameManager.isPlaying = true;
         Time.timeScale = 1;
         OnGamePaused();
     }
 
-    public void OnGamePaused(){
-        if(!gameManager.isPlaying){
+    public void OnGamePaused()
+    {
+        if (!gameManager.isPlaying)
+        {
             GameScreenCanvas.SetActive(false);
             GamePauseCanvas.SetActive(true);
-        } else {
+        }
+        else
+        {
             GameScreenCanvas.SetActive(true);
             GamePauseCanvas.SetActive(false);
         }
+    }
+
+    private void SetPanelTransparency(float alpha)
+    {
+        // Get the color of the panel's image
+        Color color = panelImage.color;
+
+        // Set the alpha value of the color
+        color.a = alpha;
+
+        // Assign the modified color back to the panel's image
+        panelImage.color = color;
     }
 }
