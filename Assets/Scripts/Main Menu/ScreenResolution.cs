d6ebfd4fae6209b0adf4471bf6669ba4637709c1
Erick <19063989@student.hhs.nl>
2024-04-13 10:01:00 +0200

    Update ScreenResolution.cs

--- Assets/Scripts/Main Menu/ScreenResolution.cs
+++ Assets/Scripts/Main Menu/ScreenResolution.cs
@@ -5,140 +5,150 @@ using System.Collections;
 
 public class ScreenResolution : MonoBehaviour
 {
-    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    #region Variables & References
+        [Header("Screen Resolution Dropdown")]
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
 
-    private Resolution[] resolutions;
-    private List<Resolution> filteredResolutions;
-    private int currentResolutionIndex = 0;
+        // --- Screen Resolution Settings --- //
+        private Resolution[] resolutions;
+        private List<Resolution> filteredResolutions;
+        private int currentResolutionIndex = 0;
+    #endregion
 
-    void Awake()
-    {
-        MoveToPrimaryDisplayGameStart();
-    }
-
-    // Call method in start or awake
-    // Unity Bug fix when you have multiple display with Window 10
-    // It shows in display 2 instead of Primary Display number 1
-    // Unity2021 bug where the secondary monitor was being chosen when the game launches in a release build.
-    void MoveToPrimaryDisplayGameStart(){
-        #if !UNITY_EDITOR
-            StartCoroutine(MoveToPrimaryDisplay());
-        #endif
-    }
-    
-    IEnumerator MoveToPrimaryDisplay()
-    {
-        List<DisplayInfo> displays = new();
-        Screen.GetDisplayLayout(displays);
-        if (displays?.Count > 0)
+    #region Default Unity
+        void Awake()
         {
-            var moveOperation = Screen.MoveMainWindowTo(displays[0], new Vector2Int(displays[0].width / 2, displays[0].height / 2));
-            yield return moveOperation;
+            MoveToPrimaryDisplayGameStart();
         }
-    }
 
-    [System.Obsolete]
-    private void Start()
-    {
-        InitializeResolutionOptions();
-        resolutionDropdown.onValueChanged.AddListener(OnResolutionChanged);
-    }
-
-    [System.Obsolete]
-    private void InitializeResolutionOptions()
-    {
-        resolutions = Screen.resolutions;
-        filteredResolutions = new List<Resolution>();
-
-        resolutionDropdown.ClearOptions();
-
-        if (resolutions != null && resolutions.Length > 0)
+        [System.Obsolete]
+        private void Start()
         {
-            int screenWidth = PlayerPrefs.GetInt("ScreenWidth", Screen.currentResolution.width);
-            int screenHeight = PlayerPrefs.GetInt("ScreenHeight", Screen.currentResolution.height);
-            int currentRefreshRate = Screen.currentResolution.refreshRate;
+            InitializeResolutionOptions();
+            resolutionDropdown.onValueChanged.AddListener(OnResolutionChanged);
+        }
+    #endregion
 
-            foreach (Resolution res in resolutions)
+    #region Force Main Display
+        // Call method in start or awake
+        // Unity Bug fix when you have multiple display with Window 10
+        // It shows in display 2 instead of Primary Display number 1
+        // Unity2021 bug where the secondary monitor was being chosen when the game launches in a release build.
+        void MoveToPrimaryDisplayGameStart(){
+            #if !UNITY_EDITOR
+                StartCoroutine(MoveToPrimaryDisplay());
+            #endif
+        }
+        
+        IEnumerator MoveToPrimaryDisplay()
+        {
+            List<DisplayInfo> displays = new();
+            Screen.GetDisplayLayout(displays);
+            if (displays?.Count > 0)
             {
-                if (res.refreshRate == currentRefreshRate)
-                {
-                    filteredResolutions.Add(res);
-                }
+                var moveOperation = Screen.MoveMainWindowTo(displays[0], new Vector2Int(displays[0].width / 2, displays[0].height / 2));
+                yield return moveOperation;
             }
+        }
+    #endregion
 
-            List<string> options = new List<string>();
+    #region Resolution Functions
+        [System.Obsolete]
+        private void InitializeResolutionOptions()
+        {
+            resolutions = Screen.resolutions;
+            filteredResolutions = new List<Resolution>();
 
-            foreach (Resolution res in filteredResolutions)
+            resolutionDropdown.ClearOptions();
+
+            if (resolutions != null && resolutions.Length > 0)
             {
-                string aspectRatio = GetAspectRatio(res.width, res.height);
-                string resolutionOption = $"{res.width}x{res.height} ({aspectRatio}) {res.refreshRate} Hz";
-                options.Add(resolutionOption);
+                int screenWidth = PlayerPrefs.GetInt("ScreenWidth", Screen.currentResolution.width);
+                int screenHeight = PlayerPrefs.GetInt("ScreenHeight", Screen.currentResolution.height);
+                int currentRefreshRate = Screen.currentResolution.refreshRate;
 
-                if (res.width == screenWidth && res.height == screenHeight)
+                foreach (Resolution res in resolutions)
                 {
-                    currentResolutionIndex = filteredResolutions.IndexOf(res);
+                    if (res.refreshRate == currentRefreshRate)
+                    {
+                        filteredResolutions.Add(res);
+                    }
                 }
+
+                List<string> options = new List<string>();
+
+                foreach (Resolution res in filteredResolutions)
+                {
+                    string aspectRatio = GetAspectRatio(res.width, res.height);
+                    string resolutionOption = $"{res.width}x{res.height} ({aspectRatio}) {res.refreshRate} Hz";
+                    options.Add(resolutionOption);
+
+                    if (res.width == screenWidth && res.height == screenHeight)
+                    {
+                        currentResolutionIndex = filteredResolutions.IndexOf(res);
+                    }
+                }
+
+                resolutionDropdown.AddOptions(options);
+                resolutionDropdown.value = currentResolutionIndex;
+                resolutionDropdown.RefreshShownValue();
+
+                SetResolution(currentResolutionIndex);
             }
-
-            resolutionDropdown.AddOptions(options);
-            resolutionDropdown.value = currentResolutionIndex;
-            resolutionDropdown.RefreshShownValue();
-
-            SetResolution(currentResolutionIndex);
+            else
+            {
+                Debug.LogWarning("No resolutions found.");
+            }
         }
-        else
+
+        private string GetAspectRatio(int width, int height)
         {
-            Debug.LogWarning("No resolutions found.");
+            float aspectRatio = (float)width / height;
+            return aspectRatio.ToString("0.##");
         }
-    }
 
-    private string GetAspectRatio(int width, int height)
-    {
-        float aspectRatio = (float)width / height;
-        return aspectRatio.ToString("0.##");
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        if (resolutionIndex >= 0 && resolutionIndex < filteredResolutions.Count)
+        public void SetResolution(int resolutionIndex)
         {
-            Resolution resolution = filteredResolutions[resolutionIndex];
-            Screen.SetResolution(resolution.width, resolution.height, true);
+            if (resolutionIndex >= 0 && resolutionIndex < filteredResolutions.Count)
+            {
+                Resolution resolution = filteredResolutions[resolutionIndex];
+                Screen.SetResolution(resolution.width, resolution.height, true);
 
-            PlayerPrefs.SetInt("ScreenWidth", resolution.width);
-            PlayerPrefs.SetInt("ScreenHeight", resolution.height);
-            PlayerPrefs.Save();
+                PlayerPrefs.SetInt("ScreenWidth", resolution.width);
+                PlayerPrefs.SetInt("ScreenHeight", resolution.height);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                Debug.LogWarning("Invalid resolution index.");
+            }
         }
-        else
+
+        private void OnResolutionChanged(int resolutionIndex)
         {
-            Debug.LogWarning("Invalid resolution index.");
+            SetResolution(resolutionIndex);
         }
-    }
 
-    private void OnResolutionChanged(int resolutionIndex)
-    {
-        SetResolution(resolutionIndex);
-    }
-
-    private void OnApplicationQuit()
-    {
-        SaveResolution();
-    }
-
-    private void OnApplicationPause(bool pauseStatus)
-    {
-        if (pauseStatus)
+        private void OnApplicationQuit()
         {
             SaveResolution();
         }
-    }
 
-    private void SaveResolution()
-    {
-        int screenWidth = Screen.currentResolution.width;
-        int screenHeight = Screen.currentResolution.height;
-        PlayerPrefs.SetInt("ScreenWidth", screenWidth);
-        PlayerPrefs.SetInt("ScreenHeight", screenHeight);
-        PlayerPrefs.Save();
-    }
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            if (pauseStatus)
+            {
+                SaveResolution();
+            }
+        }
+
+        private void SaveResolution()
+        {
+            int screenWidth = Screen.currentResolution.width;
+            int screenHeight = Screen.currentResolution.height;
+            PlayerPrefs.SetInt("ScreenWidth", screenWidth);
+            PlayerPrefs.SetInt("ScreenHeight", screenHeight);
+            PlayerPrefs.Save();
+        }
+    #endregion
 }
