3fb5b24d9d6dbe8e58772575e7a568dec4561386
Erick <19063989@student.hhs.nl>
2024-04-13 10:06:45 +0200

    Update AssignedBinding.cs

--- Assets/Scripts/Main Menu/Keybinding/AssignedBinding.cs
+++ Assets/Scripts/Main Menu/Keybinding/AssignedBinding.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// This is to reference which scirptable object will be binded.
+// Drag this into a the Button
 public class AssignedBinding : MonoBehaviour
 {
     public KeybindingSO binding;
